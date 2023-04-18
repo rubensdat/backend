dd4c3364b080a4e5b83dc323c811605a3b9e0605
Rubens Serafim do Nascimento <131165165+rubensdat@users.noreply.github.com>
2023-04-18 10:01:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.json({
+        message: "Olá"
+    });
+});
+
+const PORT = 3008;
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log('Rodando na porta ' + PORT);
+})
